018d0cb552180e6f7a21e05d57c243407de9ee26
Armando Enrique PIsfil Puemape <armandoaepp@gmail.com>
2018-11-10 19:27:59 -0500

    dev

---
+++ public/app/beans/BeanCategoria.php
@@ -0,0 +1,94 @@
+<?php 
+
+/**
+ * [Class Bean Generada]
+ * Autor: Armando E. Pisfil Puemape
+ * twitter: @armandoaepp
+ * email: armandoaepp@gmail.com
+*/
+
+class BeanCategoria{
+    # Constructor
+    public function __construct(){}
+
+    # Atributos
+    private $idcategoria;
+    private $nombre;
+    private $url = "" ;
+    private $imagen = "" ;
+    private $publicar = "S" ;
+    private $estado = 1 ;
+    private $created_up = NULL ;
+
+    # METODOS
+    public function setIdcategoria($idcategoria_)
+    {
+        $this->idcategoria = Validation::validate( $idcategoria_ );
+    }
+
+    public function getIdcategoria()
+    {
+        return $this->idcategoria;
+    }
+
+    public function setNombre($nombre_)
+    {
+        $this->nombre = Validation::validate( $nombre_ );
+    }
+
+    public function getNombre()
+    {
+        return $this->nombre;
+    }
+
+    public function setUrl($url_)
+    {
+        $this->url = Validation::validate( $url_ );
+    }
+
+    public function getUrl()
+    {
+        return $this->url;
+    }
+
+    public function setImagen($imagen_)
+    {
+        $this->imagen = Validation::validate( $imagen_ );
+    }
+
+    public function getImagen()
+    {
+        return $this->imagen;
+    }
+
+    public function setPublicar($publicar_)
+    {
+        $this->publicar = Validation::validate( $publicar_ );
+    }
+
+    public function getPublicar()
+    {
+        return $this->publicar;
+    }
+
+    public function setEstado($estado_)
+    {
+        $this->estado = Validation::validate( $estado_ );
+    }
+
+    public function getEstado()
+    {
+        return $this->estado;
+    }
+
+    public function setCreatedUp($created_up_)
+    {
+        $this->created_up = Validation::validate( $created_up_ );
+    }
+
+    public function getCreatedUp()
+    {
+        return $this->created_up;
+    }
+
+}
